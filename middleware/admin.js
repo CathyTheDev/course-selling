054b961e847f3a0476fc4c076e9a1266180becbb
CathyTheDev <jasonathy99@gmail.com>
2025-03-24 04:57:17 +0530

    Add files via upload

---
+++ middleware/admin.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+const jwt = require("jsonwebtoken");
+
+import { JWT_ADMIN_PASSWORD } from '../config';
+const {JWT_ADMIN_PASSWORD} = require("../config");
+
+function adminMiddleware(req,res,next){
+    const token = req.headers.token;
+
+    try{
+        const verified = jwt.verify(token, JWT_ADMIN_PASSWORD);
+
+        req.adminId = verified.id;
+
+        next();
+    } catch(e){
+        return res.status(403).json({
+            message: "You are not signed in!"
+        });
+    }
+}
+
+module.exports = {
+    adminMiddleware: adminMiddleware
+}
---
+++ middleware/user.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+const jwt = require("jsonwebtoken");
+
+import { JWT_USER_PASSWORD } from '../config';
+const {JWT_USER_PASSWORD} = require("../config");
+
+function userMiddleware(req, res, next){
+    const token = req.headers.token;
+
+    try{
+        const decoded = jwt.verify(token,JWT_USER_PASSWORD);
+
+        req.userId = decoded.id;
+
+        next();
+    }catch(e){
+        return res.status(403).json({
+            message: "You are not signed in!"
+        });
+    }
+}
+
+module.exports = {
+    userMiddleware: userMiddleware
+}
